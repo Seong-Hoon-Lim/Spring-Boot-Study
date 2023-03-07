d883102ce8c0f959b60f44d6f0fc2d3b07c82e69
Seong-Hoon-Lim <108711069+Seong-Hoon-Lim@users.noreply.github.com>
2023-03-08 00:02:06 +0900

    Spring-Boot 쇼핑몰 프로젝트
    Spring-Boot 쇼핑몰 프로젝트 with JPA - 변구훈

--- shoppingmall/src/main/java/com/hooneystory/shoppingmall/repository/ItemRepository.java
+++ shoppingmall/src/main/java/com/hooneystory/shoppingmall/repository/ItemRepository.java
@@ -2,7 +2,67 @@ package com.hooneystory.shoppingmall.repository;
 
 import com.hooneystory.shoppingmall.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    /**
+     * 상품명을 이용하여 데이터를 조회하는 쿼리 메소드
+     * 매개변수로는 검색할 때 사용할 상품명 변수를 전달
+     * @param itemNm
+     * @return
+     */
+    List<Item> findByItemNm(String itemNm);
+
+    /**
+     * 상품명과 상품 상세 설명을 OR 조건을 이용하여
+     * 조회하는 쿼리 메소드
+     * @param itemNm
+     * @param itemDetail
+     * @return
+     */
+    List<Item> findByItemNmOrItemDetail(String itemNm, String itemDetail);
+
+    /**
+     * 파라미터로 넘어온 price 변수보다 값이 작은 상품 데이터를
+     * 조회하는 쿼리 메소드
+     * @param price
+     * @return
+     */
+    List<Item> findByPriceLessThan(Integer price);
+
+    /**
+     * LessThan 으로 출력된 결과를 오름차순으로 정렬 조회
+     * 상품의 가격이 높은 순으로 조회하는 쿼리 메소드
+     * @param price
+     * @return
+     */
+    List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
+
+    /**
+     * @Query 어노테이션을 이용하여 상품 데이터를 조회하는 쿼리 메소드
+     * 상품 상세 설명을 파라미터로 받아 해당 내용을 상품 상세 설명에 포함하고 있는
+     * 데이터를 조회하며, 정렬 순서는 가격이 높은 순으로 조회
+     * @param itemDetail
+     * @return
+     */
+    @Query("select i from Item i where i.itemDetail like %:itemDetail% order by i.price desc")
+    //JPQL 로 작성한 쿼리문 from 뒤에는 엔티티 클래스 Item을 i 로 지정 하고 Item으로부터 데이터를 select 한다.
+    List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
+    //파라미터에 @Param 어노테이션을 이용하여 itemDetail 변수를 "like % % 사이에 :itemDetail 로 지정"
+
+    /**
+     * 기존 데이터베이스에서 사용하던 쿼리를 그대로 사용해야 할 떄
+     * @Query의 nativeQuery 속성을 사용하여 기존 쿼리를 그대로 사용하는 쿼리 메소드
+     * 특정 데이터베이스에 종속되기 때문에 복잡한 통계용 쿼리에 활용하는 것이 좋다
+     * @param itemDetail
+     * @return
+     */
+    @Query(value = "select * from item i where i.item_detail like %:itemDetail% order by i.price desc"
+                       , nativeQuery = true)
+    List<Item> findByItemDetailByNative(@Param("itemDetail") String itemDetail);
 
 }
---
+++ shoppingmall/src/test/java/com/hooneystory/shoppingmall/repository/ItemRepositoryTest.java
@@ -0,0 +1,111 @@
+package com.hooneystory.shoppingmall.repository;
+
+import com.hooneystory.shoppingmall.constant.ItemSellStatus;
+import com.hooneystory.shoppingmall.entity.Item;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest //통합 테스트를 위해 스프링 부트에서 제공하는 어노테이션 실제 애플리케이션 구동과 마찬가지로 IoC 컨테이너에 모든 Bean을 등록
+@TestPropertySource(locations = "classpath:application-test.properties")
+class ItemRepositoryTest {
+
+    @Autowired
+    ItemRepository itemRepository;
+
+    @Test   //테스트 할 메소드 위에 선언하여 해당 메소드를 테스트 대상으로 지정
+    @DisplayName("상품 저장 테스트")
+    public void createItemTest() {
+        Item item = new Item();
+        item.setItemNm("테스트 상품");
+        item.setPrice(10000);
+        item.setItemDetail("테스트 상품 상세 설명");
+        item.setItemSellStatus(ItemSellStatus.SELL);
+        item.setStockNumber(100);
+        item.setRegTime(LocalDateTime.now());
+        item.setUpdateTime(LocalDateTime.now());
+        Item savedItem = itemRepository.save(item);
+        System.out.println(savedItem.toString());
+    }
+
+    public void createItemList() {
+        for (int i = 1; i <= 10; i++) {
+            Item item = new Item();
+            item.setItemNm("테스트 상품" +i);
+            item.setPrice(10000 + i);
+            item.setItemDetail("테스트 상품 상세 설명" + i);
+            item.setItemSellStatus(ItemSellStatus.SELL);
+            item.setStockNumber(100);
+            item.setRegTime(LocalDateTime.now());
+            item.setUpdateTime(LocalDateTime.now());
+            Item savedItem = itemRepository.save(item);
+        }
+    }
+
+    @Test
+    @DisplayName("상품명 조회 테스트")
+    public void findByItemNmTest() {
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByItemNm("테스트 상품1");
+        for (Item item: itemList) {
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("상품명, 상품상세설명 or 테스트")
+    public void findByItemNmOrItemDetailTest() {
+        this.createItemList();
+        List<Item> itemList =
+                    itemRepository.findByItemNmOrItemDetail("테스트 상품1", "테스트 상품 상세 설명5");
+        for (Item item : itemList) {
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("가격 LessThan 테스트")
+    public void findByPriceLessThanTest() {
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByPriceLessThan(10005);
+        for (Item item : itemList) {
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("가격 내림차순 조회 테스트")
+    public void findByPriceLessThanOrderByPriceDesc() {
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByPriceLessThanOrderByPriceDesc(10005);
+        for(Item item : itemList) {
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("@Query를 이용한 상품 조회 테스트")
+    public void findByItemDetailTest() {
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByItemDetail("테스트 상품 상세 설명");
+        for (Item item : itemList) {
+            System.out.println(item.toString());
+        }
+    }
+
+    public void findByItemDetailByNative() {
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByItemDetailByNative("테스트 상품 상세 설명");
+        for (Item item : itemList) {
+            System.out.println(item.toString());
+        }
+    }
+
+}

e208ca6b00c9d4a74885a911bdb184b0d98d84c8
Seong-Hoon-Lim <108711069+Seong-Hoon-Lim@users.noreply.github.com>
2023-03-08 23:36:24 +0900

    Spring-Boot 쇼핑몰 프로젝트
    Spring-Boot 쇼핑몰 프로젝트 with JPA - 변구훈

---
+++ shoppingmall/src/main/java/com/hooneystory/shoppingmall/controller/ThymeleafExController.java
@@ -0,0 +1,110 @@
+package com.hooneystory.shoppingmall.controller;
+
+import com.hooneystory.shoppingmall.dto.ItemDto;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping(value = "/thymeleaf")
+public class ThymeleafExController {
+
+    @GetMapping(value = "/ex01")
+    public String thymeleafExample01(Model model) {
+        model.addAttribute("data", "타임리프 예제입니다.");
+        return "thymeleafEx/thymeleafEx01";
+    }
+
+    /**
+     * th:text 를 이용한 상품 데이터 출력용 컨트롤러
+     * @param model
+     * @return
+     */
+    @GetMapping(value = "/ex02")
+    public String thymeleafExample02(Model model) {
+        ItemDto itemDto = new ItemDto();
+        itemDto.setItemDetail("상품 상세 설명");
+        itemDto.setItemNm("테스트 상품1");
+        itemDto.setPrice(10000);
+        itemDto.setRegTime(LocalDateTime.now());
+
+        model.addAttribute("itemDto", itemDto);
+        return "thymeleafEx/thymeleafEx02";
+    }
+
+    /**
+     * th:each 를 이용한 상품 리스트 출력용 컨트롤러
+     * @param model
+     * @return
+     */
+    @GetMapping(value = "/ex03")
+    public String thymeleafExample03(Model model) {
+
+        List<ItemDto> itemDtoList = new ArrayList<>();
+
+        for(int i = 1; i <= 10; i++) {
+            ItemDto itemDto = new ItemDto();
+            itemDto.setItemDetail("상품 상세 설명" + i);
+            itemDto.setItemNm("테스트 상품" + i);
+            itemDto.setPrice(1000 * i);
+            itemDto.setRegTime(LocalDateTime.now());
+
+            itemDtoList.add(itemDto);
+        }
+        model.addAttribute("itemDtoList", itemDtoList);
+        return "thymeleafEx/thymeleafEx03";
+    }
+
+    /**
+     * th:if, th:unless (단일 조건)
+     * th:sqitch, th:case (여러 조건)
+     * 들을 활용한 조건문 처리용 컨트롤러
+     * @param model
+     * @return
+     */
+    @GetMapping(value = "/ex04")
+    public String thymeleafExample04(Model model) {
+
+        List<ItemDto> itemDtoList = new ArrayList<>();
+
+        for(int i = 1; i <= 10; i++) {
+            ItemDto itemDto = new ItemDto();
+            itemDto.setItemDetail("상품 상세 설명" + i);
+            itemDto.setItemNm("테스트 상품" + i);
+            itemDto.setPrice(1000 * i);
+            itemDto.setRegTime(LocalDateTime.now());
+
+            itemDtoList.add(itemDto);
+        }
+        model.addAttribute("itemDtoList", itemDtoList);
+        return "thymeleafEx/thymeleafEx04";
+    }
+
+    /**
+     * th:href 를 이용한 링크 처리용 컨트롤러
+     * @return
+     */
+    @GetMapping(value = "/ex05")
+    public String thymeleafExample05() {
+        return "thymeleafEx/thymeleafEx05";
+    }
+
+    /**
+     * th:href 를 이용한 파라미터 데이터 전달용 컨트롤러
+     * @param param1
+     * @param param2
+     * @param model
+     * @return
+     */
+    @GetMapping(value = "/ex06")
+    public String thymeleafExample06(String param1, String param2, Model model) {
+        model.addAttribute("param1", param1);
+        model.addAttribute("param2", param2);
+        return "thymeleafEx/thymeleafEx06";
+    }
+}
--- shoppingmall/src/test/java/com/hooneystory/shoppingmall/repository/ItemRepositoryTest.java
+++ shoppingmall/src/test/java/com/hooneystory/shoppingmall/repository/ItemRepositoryTest.java
@@ -2,12 +2,23 @@ package com.hooneystory.shoppingmall.repository;
 
 import com.hooneystory.shoppingmall.constant.ItemSellStatus;
 import com.hooneystory.shoppingmall.entity.Item;
+import com.hooneystory.shoppingmall.entity.QItem;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.TestPropertySource;
+import org.thymeleaf.util.StringUtils;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.print.attribute.standard.PageRanges;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -19,6 +30,9 @@ class ItemRepositoryTest {
 
     @Autowired
     ItemRepository itemRepository;
+    
+    @PersistenceContext //영속성 컨텍스트를 사용하기 위해 @PersistenceContext 어노테이션을 활용하여 EntityManager 빈을 주입
+    EntityManager em;
 
     @Test   //테스트 할 메소드 위에 선언하여 해당 메소드를 테스트 대상으로 지정
     @DisplayName("상품 저장 테스트")
@@ -46,6 +60,32 @@ class ItemRepositoryTest {
             item.setRegTime(LocalDateTime.now());
             item.setUpdateTime(LocalDateTime.now());
             Item savedItem = itemRepository.save(item);
+        }
+    }
+
+    public void createItemList2() {
+        for(int i = 1; i <= 5; i++) {
+            Item item = new Item();
+            item.setItemNm("테스트 상품" + i);
+            item.setPrice(10000 + i);
+            item.setItemDetail("테스트 상품 상세 설명" + i);
+            item.setItemSellStatus(ItemSellStatus.SELL);
+            item.setStockNumber(100);
+            item.setRegTime(LocalDateTime.now());
+            item.setUpdateTime(LocalDateTime.now());
+            itemRepository.save(item);
+        }
+
+        for(int i = 6; i <= 10; i++) {
+            Item item = new Item();
+            item.setItemNm("테스트 상품" + i);
+            item.setPrice(10000 + i);
+            item.setItemDetail("테스트 상품 상세 설명" + i);
+            item.setItemSellStatus(ItemSellStatus.SOLD_OUT);
+            item.setStockNumber(0);
+            item.setRegTime(LocalDateTime.now());
+            item.setUpdateTime(LocalDateTime.now());
+            itemRepository.save(item);
         }
     }
 
@@ -105,6 +145,51 @@ class ItemRepositoryTest {
         List<Item> itemList = itemRepository.findByItemDetailByNative("테스트 상품 상세 설명");
         for (Item item : itemList) {
             System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("Querydsl 조회 테스트1")
+    public void queryDslTest() {
+        this.createItemList();
+        JPAQueryFactory queryFactory = new JPAQueryFactory(em); //JPAQueryFacotry 를 이용하여 쿼리르 동적 생성
+        QItem qItem = QItem.item;   //Querydsl을 통해 쿼리를 생성하기 위해 플러그인을 통해 생성된 QItem 객체를 활용
+        JPAQuery<Item> query = queryFactory.selectFrom(qItem)
+                .where(qItem.itemSellStatus.eq(ItemSellStatus.SELL))
+                .where(qItem.itemDetail.like("%" + "테스트 상품 상세 설명" + "%"))
+                .orderBy(qItem.price.desc());
+
+        List<Item> itemList = query.fetch();    //쿼리를 결과를 리스트로 반환
+        for(Item item : itemList) {
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("상품 Querydsl 조회 테스트 2")
+    public void queryDslTest2() {
+        this.createItemList();
+        BooleanBuilder booleanBuilder = new BooleanBuilder();
+        QItem item = QItem.item;
+        String itemDetail = "테스트 상품 상세 설명";
+        int price = 10003;
+        String itemSellStat = "SELL";
+
+        booleanBuilder.and(item.itemDetail.like("%" + itemDetail + "%"));
+        booleanBuilder.and(item.price.gt(price));
+
+        if(StringUtils.equals(itemSellStat, ItemSellStatus.SELL)) {
+            booleanBuilder.and(item.itemSellStatus.eq(ItemSellStatus.SELL));
+        }
+
+        Pageable pageable = PageRequest.of(0, 5);
+        Page<Item> itemPagingResult =
+                itemRepository.findAll(booleanBuilder, pageable);
+        System.out.println("total elements : " +
+                            itemPagingResult.getTotalElements());
+        List<Item> resultItemList = itemPagingResult.getContent();
+        for(Item resultItem : resultItemList) {
+            System.out.println(resultItem.toString());
         }
     }
 
